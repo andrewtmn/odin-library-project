2ffe619688b66025d19c024dbb85f11dd64ec371
andrew <51986515+andrewtmn@users.noreply.github.com>
2021-07-07 17:27:12 +1000

    implemented remove book functionality
    Not as efficient, we need to reload the entire library into the table when a book is removed or toggling the read status

--- library.js
+++ library.js
@@ -8,7 +8,6 @@ Date.prototype.yyyymmdd = function() {
   };
 
 let library = [];
-let count = 0;
 
 function Book(name, author, publishDate, numPages) {
     this.name = name;
@@ -23,10 +22,18 @@ function addBookToLibrary(name, author, publishDate, numPages) {
     library.push(book);
 }
 
+// Displays any books in library that haven't been displayed yet.
+// Global variable count keeps track of this. 
 function displayBooksInLibrary() {
     console.log('called');
     let display = document.getElementById("book-display");
-    for (let i = count; i < library.length; i++) {
+    
+    // clear everything except headers
+    let headers = display.firstElementChild;
+    display.innerHTML = '';
+    display.appendChild(headers);
+
+    for (let i = 0; i < library.length; i++) {
         let book = document.createElement("div");
         book.setAttribute("class", "book-table-item");
         book.setAttribute("id", library[i].name);
@@ -37,7 +44,6 @@ function displayBooksInLibrary() {
         addRemoveBookBtn(book);
 
         display.appendChild(book);
-        count++;
     }
 
     document.documentElement.style.setProperty("--rowNum", library.length);
@@ -59,20 +65,23 @@ function addRemoveBookBtn(book) {
 
 function addAttributesToBookEntry(bookEntry, book) {
     let bookName = document.createElement("p");
+    bookName.setAttribute("class", "book-name");
     bookName.innerText = book.name;
     let bookAuthor = document.createElement("p");
+    bookAuthor.setAttribute("class", "author");
     bookAuthor.innerText = book.author;
     let bookPublishDate = document.createElement("p");
+    bookPublishDate.setAttribute("class", "publish-date");
     if (book.publishDate instanceof Date) {
-        console.log("instance");
-        bookPublishDate.innerText =
-            book.publishDate.yyyymmdd();
+        bookPublishDate.innerText = book.publishDate.yyyymmdd();
     } else {
         bookPublishDate.innerText = book.publishDate;
     }
     let bookPages = document.createElement("p");
+    bookPages.setAttribute("class", "num-pages");
     bookPages.innerText = book.numPages;
     let bookRead = document.createElement("p");
+    bookRead.setAttribute("class", "read-status");
     let readStatus = book.readStatus;
     bookRead.innerText = readStatus ? "Read" : "Not Read Yet";
 
@@ -90,9 +99,11 @@ function addNewBook() {
     let author = document.getElementById("author");
     let date = document.getElementById("date");
     let pages = document.getElementById("pages");
-    console.log("got elements", name.value, author.value, date.value, pages.value);
     addBookToLibrary(name.value, author.value, date.value, pages.value);
-    displayBooksInLibrary();
+
+    reloadTable();
+
+    // clear the form
     name.value = '';
     author.value = '';
     date.value = '';
@@ -100,21 +111,67 @@ function addNewBook() {
     closeForm();
 }
 
+function reloadTable() {
+    displayBooksInLibrary();
+    addReadToggleHandler();
+    addRemoveBookHandler();
+}
+
 const newBookBtn = document.querySelector('#new-book-btn');
 newBookBtn.addEventListener('click', openForm);
+
+const cancelBtn = document.getElementById("cancel-new-book");
+cancelBtn.addEventListener('click', closeForm);
 
 function openForm() {
     document.getElementById("new-book-popup").style.display = "block";
 }
 
-const cancelBtn = document.getElementById("cancel-new-book");
-cancelBtn.addEventListener('click', closeForm);
-
 function closeForm() {
     document.getElementById("new-book-popup").style.display = "none";
+}
+
+function addReadToggleHandler() {
+    let readBtns = document.getElementsByClassName("toggle-read-btn");
+    console.log(readBtns);
+    for (let i = 0; i < readBtns.length; i++) {
+        console.log('adding event listener to book');
+        readBtns[i].addEventListener('click', function() {
+            console.log("before toggle");
+            toggleReadStatus(readBtns[i]);
+        });
+    }
+}
+
+function toggleReadStatus(btn) {
+    let book = btn.parentElement;
+    let read = book.querySelector('.read-status');
+    console.log(read);
+    if (read.innerText == "Not Read Yet") {
+        read.innerText = "Read";
+    } else {
+        read.innerText = "Not Read Yet";
+    }
+}
+
+function addRemoveBookHandler() {
+    let removeBtns = document.getElementsByClassName("remove-book-btn");
+    console.log
+    for (let i = 0; i < removeBtns.length; i++) {
+        removeBtns[i].addEventListener('click', function() {
+            removeBook(removeBtns[i]);
+        });
+    }
+}
+
+function removeBook(removeBtn) {
+    let book = removeBtn.parentElement;
+    let index = book.dataset.index;
+    library = library.slice(0, index).concat(library.slice(index+1));
+    reloadTable();
 }
 
 addBookToLibrary("Clean Code", "author", new Date(2008, 7, 1), 800);
 addBookToLibrary("The Pragmatic Programmer", "author", new Date(1999, 9, 1), 750);
 
-displayBooksInLibrary();
+reloadTable();
